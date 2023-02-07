f4040da6b32bda534fac68538d1cfab4fa7ab07a
Bárbara <barbara.yasmin14@gmail.com>
2023-02-07 19:48:12 -0300

    formatei o resultado da divisão para uma melhor visualização do resultado e mudei o nome de algumas variaveis para um melhor entendimento do código

--- opMatematicas.js
+++ opMatematicas.js
@@ -1,27 +1,27 @@
 
-var result=document.getElementById('result')
+var campoDeResultado=document.getElementById('campoDeResultado')
 
 function soma(){
     var n1=Number(document.getElementById('n1').value)
     var n2=Number(document.getElementById('n2').value)
-    var s=n1+n2
-    result.innerText=s
+    var resultado=n1+n2
+    campoDeResultado.innerText=resultado
 }
 function sub(){
     var n1=Number(document.getElementById('n1').value)
     var n2=Number(document.getElementById('n2').value)
-    var s=n1-n2
-    result.innerText=s
+    var resultado=n1-n2
+    campoDeResultado.innerText=resultado
 }
 function div(){
     var n1=Number(document.getElementById('n1').value)
     var n2=Number(document.getElementById('n2').value)
-    var d=n1/n2
-    result.innerText=d
+    var resultado=n1/n2
+    campoDeResultado.innerText=(resultado.toFixed(2))
 }
 function mult(){
     var n1=Number(document.getElementById('n1').value)
     var n2=Number(document.getElementById('n2').value)
-    var m=n1*n2
-    result.innerText=m
+    var resultado=n1*n2
+    campoDeResultado.innerText=resultado
 }
